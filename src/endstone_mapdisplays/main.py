824cb69415a6dba860194dee2c4961f066e62007
chalupa_muntinlupa <181960037+chalupa-muntinlupa@users.noreply.github.com>
2026-03-26 19:11:19 +0800

    Add renderer

--- src/endstone_mapdisplays/main.py
+++ src/endstone_mapdisplays/main.py
@@ -1,8 +1,36 @@
-from endstone import ColorFormat
+from endstone import ColorFormat, Logger, Player
 from endstone.event import event_handler, PlayerJoinEvent, PlayerChatEvent
+from endstone.map import MapRenderer, MapCanvas, MapView
 from endstone.plugin import Plugin
+import threading
+import numpy as np
+from typing import cast, Any
 
-class TemplatePlugin(Plugin):
+class CabinetMapRenderer:
+    """
+    Renderer for a single "cabinet" *(singular map within a bigger MapDisplay)*.
+    """
+    MAP_SIZE = 128
+
+    def __init__(self, logger: Logger) -> None:
+        self.logger = logger
+        self.lock = threading.Lock()
+        self.buffer = np.zeros((self.MAP_SIZE, self.MAP_SIZE, 4), dtype=np.uint8)
+
+        self._has_frame = False
+
+    def update(self, array: np.ndarray):
+        with self.lock:
+            np.copyto(self.buffer, array)
+            self._has_frame = True
+
+    def render(self, view: MapView, canvas: MapCanvas, player: Player) -> None:
+        with self.lock:
+            if self._has_frame:
+                canvas.draw_image(0, 0, cast(Any, self.buffer))
+            else:
+                self.logger.warning("render() called with no frame available")
+
+class EntryForPlugin(Plugin):
     def on_enable(self) -> None:
-        self.logger.info("on_enable is called!")
         self.register_events(self)
